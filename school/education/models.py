4c8fb452b3e2a52d6ac40bbbf0dce11336c706b4
happyguest666top <145270176+happyguest666top@users.noreply.github.com>
2026-01-12 21:51:54 +0200

    Add files via upload

--- school/education/models.py
+++ school/education/models.py
@@ -30,3 +30,44 @@ class Classroom(models.Model):
 
     def __str__(self):
         return self.number
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Lesson(models.Model):
+    DAY_CHOICES = (
+        (1, 'Понеділок'),
+        (2, 'Вівторок'),
+        (3, 'Середа'),
+        (4, 'Четверг'),
+        (5, 'П`ятниця')
+    )
+
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    day = models.IntegerField(choices=DAY_CHOICES)
+    lesson_number = models.IntegerField()
+    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+    classroom = models.ForeignKey(Classroom, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        unique_together = ('group', 'day', 'lesson_number')
+
+    def __str__(self):
+        return f"{self.group} | {self.subject} | {self.day} | {self.lesson_number}"
+
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.DO_NOTHING)
+    value = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student} - {self.value}"
